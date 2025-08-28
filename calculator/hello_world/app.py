39e27d96eed47b7f46fae89b0755d530b57ecca1
kaori <kaorianeth.gonzalezaguilar@gmail.com>
2025-08-27 21:17:26 -0500

    Taller2

---
+++ calculator/hello_world/app.py
@@ -0,0 +1,32 @@
+import json
+
+# import requests
+def calculator (a: int, b: int, c: str):
+    if c == "suma":
+        return a + b
+    elif c == "resta":
+        return a - b
+    elif c == "multiplicacion":
+        return a * b
+    elif c == "division":
+        return a / b
+    
+    else:
+        return "no contemplado"
+    
+
+def lambda_handler(event, context):
+    calculator_data = event["calculator"]
+    a = calculator_data["numero1"]
+    b = calculator_data["numero2"]
+    funcion = calculator_data["funcion"]
+    print(f'Numero 1: {a}, Numero 2: {b}, Funcion{funcion}')
+    calc = calculator(int(a), int(b), funcion)
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": f"{calc}",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
